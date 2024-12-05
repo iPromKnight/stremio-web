4e79ab3e6a51a45cefa355264db5110538ee73c9
Tim <tymmesyde@gmail.com>
2024-12-05 11:07:15 +0100

    chore: update dependencies

--- webpack.config.js
+++ webpack.config.js
@@ -37,10 +37,6 @@ module.exports = (env, argv) => ({
                             '@babel/preset-env',
                             '@babel/preset-react'
                         ],
-                        plugins: [
-                            '@babel/plugin-proposal-class-properties',
-                            '@babel/plugin-proposal-object-rest-spread'
-                        ]
                     }
                 }
             },
